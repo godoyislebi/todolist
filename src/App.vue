069f4d5030ce91979340edfd92537206671bad8d
godoyislebi <islebi.pinto.godoy24@gmail.com>
2022-03-14 23:26:22 -0300

    ready

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="mb-5">To Do List</h1>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-12">
+          <h2>Crea una tarea</h2>
+        </div>
+        <div class="col-6">
+          <div class="input-group mb-3">
+            <input
+              type="text"
+              v-model="miTarea"
+              class="form-control"
+              placeholder=""
+              aria-label="Example text with button addon"
+              aria-describedby="button-addon1"
+            />
+            <button
+              class="btn btn-outline-secondary"
+              type="button"
+              id="button-addon1"
+              @click="agregarTarea"
+            >
+              Crear
+            </button>
+          </div>
+        </div>
+        <ul class="list-group list-group-flush">
+          <h2>Tareas</h2>
+          <li class="list-group-item my-2" v-for="(li, i) in lis" :key="i">
+            {{ li }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      miTarea: "",
+      lis: [],
+    };
+  },
+  methods: {
+    agregarTarea() {
+      this.miTarea === ""
+        ? alert("Por favor ingrese tarea")
+        : this.lis.push(this.miTarea);
+    },
   },
 };
 </script>
